55e8039d2ec29c8ffa34d18d33f17d2bb3a1ae85
Mattiax <mattia.musone.mm@gmail.com>
2018-04-23 07:52:20 +0200

    agg

---
+++ SocialZAndroid/app/src/main/java/com/example/matti/socialzandroid/MailList.java
@@ -0,0 +1,4 @@
+package com.example.matti.socialzandroid;
+
+public class MailList {
+}
---
+++ WebService/WebServices/src/java/pkg/web/SocialZ.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg.web;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import pkg.db.DBHelper;
+
+/**
+ *
+ * @author MATTI
+ */
+@WebService(serviceName = "SocialZ")
+public class SocialZ {
+/**
+     * This is a sample web service operation
+     *
+     * @param hobby
+     * @return
+     */
+    @WebMethod(operationName = "getMailList")
+    public String getMailList(@WebParam(name = "hobby") String hobby) {
+        String ris = DBHelper.getAllEmailsHobby(hobby);
+        System.out.println(ris);
+        return ris;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "getHobbies")
+    public String getHobbies() {
+        System.out.println("Getting hobbies");
+        return DBHelper.getHobbies();
+    }
+}
